2487c84ecec32491d805eaa3821513eab79b27f2
ngandung <gandungn@gmail.com>
2024-11-25 14:41:12 +0700

    introduction component

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friends.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friends.phone }}</li>
+            <li><strong>Email:</strong> {{ friends.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friends: {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '0123 456 90',
+                email: 'manuel@mail.com'
+            }
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+};
+</script>
